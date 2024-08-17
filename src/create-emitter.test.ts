68f134b2ede6a297029b6034135d52cb771d5b76
Mark Holmes <smlmrkhlms@gmail.com>
2024-08-17 02:36:55 -0600

    Fixes

---
+++ src/create-emitter.test.ts
@@ -0,0 +1,445 @@
+import { createEmitter, typeOf, Type } from './create-emitter';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+
+describe('createEmitter()', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('Returns static properties', () => {
+    const emitter = createEmitter({
+      property: 'Hello, world!',
+    });
+
+    expect(emitter).toHaveProperty('property', 'Hello, world!');
+  });
+
+  it('Returns synchronous versions of all the methods you pass into config.', () => {
+    const emitter = createEmitter({
+      first() {},
+      second() {},
+    });
+
+    expect(emitter).toHaveProperty('first');
+    expect(emitter).toHaveProperty('second');
+    expect(typeOf(emitter.first)).toEqual(Type.Function);
+    expect(typeOf(emitter.second)).toEqual(Type.Function);
+    expect(emitter.first()).toBeUndefined();
+    expect(emitter.second()).toBeUndefined();
+  });
+
+  it('Returns asynchronous versions of all the methods you pass into config.', () => {
+    const emitter = createEmitter({
+      async first() {},
+      async second() {},
+    });
+
+    expect(emitter).toHaveProperty('first');
+    expect(emitter).toHaveProperty('second');
+    expect(typeOf(emitter.first)).toEqual(Type.AsyncFunction);
+    expect(typeOf(emitter.second)).toEqual(Type.AsyncFunction);
+    expect(emitter.first()).toHaveProperty('then');
+    expect(emitter.second()).toHaveProperty('then');
+  });
+
+  it('Resolves method calls in the order in which they are called.', async () => {
+    const callStack: Array<string> = [];
+
+    const first = vi.fn(() => callStack.push('first'));
+
+    const second = vi.fn(() => callStack.push('second'));
+
+    const emitter = createEmitter({
+      first,
+      second,
+    });
+
+    emitter.first();
+    await emitter.second();
+
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).toHaveBeenCalledTimes(1);
+    expect(callStack).toMatchObject(['first', 'second']);
+  });
+
+  it('Resolves synchronous method calls in the order in which they finish executing.', async () => {
+    const callStack: Array<string> = [];
+
+    const first = () => {
+      setTimeout(() => {
+        callStack.push('first');
+      }, 1000);
+    };
+
+    const second = () => callStack.push('second');
+
+    const emitter = createEmitter({
+      first,
+      second,
+    });
+
+    emitter.first();
+
+    expect(callStack).toMatchObject([]);
+
+    vi.advanceTimersByTime(999);
+
+    emitter.second();
+
+    expect(callStack).toMatchObject(['second']);
+
+    vi.runAllTimers();
+
+    expect(callStack).toMatchObject(['second', 'first']);
+  });
+
+  it('Resolves  calls in the order in which they are called regardless of execution time.', async () => {
+    const callStack: Array<string> = [];
+
+    const first = async () =>
+      new Promise<void>((resolve) =>
+        setTimeout(() => {
+          callStack.push('first');
+          resolve();
+        }, 1000),
+      );
+
+    const second = async () => callStack.push('second');
+
+    const emitter = createEmitter({
+      first,
+      second,
+    });
+
+    emitter.first();
+
+    expect(callStack).toMatchObject([]);
+
+    emitter.second();
+
+    await vi.runAllTimersAsync();
+
+    expect(callStack).toMatchObject(['first', 'second']);
+  });
+
+  it('Initializes if an initialize method is present.', async () => {
+    const callStack: Array<string> = [];
+
+    const first = vi.fn(() => callStack.push('first'));
+
+    const initialize = vi.fn(() => callStack.push('initialize'));
+
+    const emitter = createEmitter({
+      first,
+      initialize,
+    });
+
+    await emitter.initialize();
+
+    await emitter.first();
+
+    expect(initialize).toHaveBeenCalledTimes(1);
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(callStack).toMatchObject(['initialize', 'first']);
+  });
+
+  it('Can call initialize explicitly.', async () => {
+    const callStack: Array<string> = [];
+
+    const initialize = vi.fn(() => callStack.push('initialize'));
+
+    const emitter = createEmitter({
+      initialize,
+    });
+
+    await emitter.initialize();
+
+    expect(initialize).toHaveBeenCalledTimes(1);
+    expect(callStack).toMatchObject(['initialize']);
+  });
+
+  it('Prevents sync initialize from being called multiple times.', () => {
+    const callStack: Array<string> = [];
+
+    const initialize = () => callStack.push('initialize');
+
+    const emitter = createEmitter({ initialize });
+
+    emitter.initialize();
+
+    expect(callStack).toMatchObject(['initialize']);
+
+    expect(() => emitter.initialize()).toThrow('initialize() can only be called once.');
+
+    expect(callStack).toMatchObject(['initialize']);
+  });
+
+  it('Prevents async initialize from being called multiple times.', async () => {
+    const callStack: Array<string> = [];
+
+    const initialize = async () => callStack.push('initialize');
+
+    const emitter = createEmitter({ initialize });
+
+    await emitter.initialize();
+
+    expect(callStack).toMatchObject(['initialize']);
+
+    await expect(emitter.initialize()).rejects.toThrow('initialize() can only be called once.');
+
+    expect(callStack).toMatchObject(['initialize']);
+  });
+
+  it('Exposes a getter to confirm enabled state.', async () => {
+    const first = async () => {};
+
+    const initialize = async () => {};
+
+    const emitter = createEmitter({
+      first,
+      initialize,
+    });
+
+    await emitter.initialize();
+
+    expect(emitter.enabled).toEqual(true);
+
+    emitter.disable();
+
+    expect(emitter.enabled).toEqual(false);
+  });
+
+  it('Exposes a getter to confirm flushing state.', async () => {
+    const first = async () => new Promise<void>((resolve) => setTimeout(resolve, 1000));
+
+    const initialize = async () => {};
+
+    const emitter = createEmitter({
+      first,
+      initialize,
+    });
+
+    emitter.initialize();
+    emitter.first();
+
+    expect(emitter.flushing).toEqual(true);
+
+    await vi.runAllTimersAsync();
+
+    expect(emitter.flushing).toEqual(false);
+  });
+
+  it('If an async function is flushing, we wait until its done before unsubscribing.', async () => {
+    const first = async () => new Promise<void>((resolve) => setTimeout(resolve, 1000));
+
+    const emitter = createEmitter({ first });
+
+    const unsubscribe = emitter.subscribe({ first: vi.fn(async () => {}) });
+
+    emitter.first();
+
+    unsubscribe();
+
+    expect(emitter.flushing).toEqual(true);
+    expect(Object.getOwnPropertySymbols(emitter.__SUBSCRIPTIONS__).length).toBe(1);
+
+    await vi.runAllTimersAsync();
+
+    expect(emitter.flushing).toEqual(false);
+    expect(Object.keys(emitter.__SUBSCRIPTIONS__).length).toBe(0);
+  });
+
+  it('Exposes a getter to confirm initialized state.', async () => {
+    const first = async () => {};
+
+    const initialize = async () => {};
+
+    const emitter = createEmitter({
+      first,
+      initialize,
+    });
+
+    expect(emitter.initialized).toEqual(false);
+
+    await emitter.initialize();
+
+    await emitter.first();
+
+    expect(emitter.initialized).toEqual(true);
+  });
+
+  it('Rejects if a method throws.', async () => {
+    const emitter = createEmitter({
+      async first() {
+        throw new Error(':(');
+      },
+    });
+
+    await expect(emitter.first()).rejects.toThrow(':(');
+  });
+
+  it('Adds a subscription.', async () => {
+    const emitter = createEmitter({
+      async first() {},
+    });
+
+    const first = vi.fn();
+
+    emitter.subscribe({ first });
+
+    await emitter.first();
+
+    expect(first).toHaveBeenCalledTimes(1);
+  });
+
+  it('Adds multiple subscriptions.', async () => {
+    const emitter = createEmitter({
+      async first() {},
+    });
+
+    const first = vi.fn();
+
+    emitter.subscribe({ first });
+    emitter.subscribe({ first });
+
+    await emitter.first();
+
+    expect(first).toHaveBeenCalledTimes(2);
+  });
+
+  it('Subscribes to all events.', async () => {
+    const emitter = createEmitter({
+      async first() {},
+      async second() {},
+      async third() {},
+    });
+
+    const all = vi.fn();
+
+    emitter.subscribe({ all });
+
+    await emitter.first();
+    await emitter.second();
+    await emitter.third();
+
+    expect(all).toHaveBeenCalledTimes(3);
+  });
+
+  it('Only cancels subscriptions of thrown methods.', async () => {
+    const emitter = createEmitter({
+      async first() {
+        throw new Error(':(');
+      },
+      async second() {},
+    });
+
+    const first = vi.fn();
+    const second = vi.fn();
+
+    emitter.subscribe({ first });
+    emitter.subscribe({ second });
+
+    await expect(emitter.first()).rejects.toThrow();
+
+    await emitter.second();
+
+    expect(first).toHaveBeenCalledTimes(0);
+    expect(second).toHaveBeenCalledTimes(1);
+  });
+
+  it('Unsubscribes from a method.', async () => {
+    const emitter = createEmitter({
+      async first() {},
+    });
+
+    const first = vi.fn();
+
+    const unsubscribe = emitter.subscribe({ first });
+
+    await emitter.first();
+
+    expect(first).toHaveBeenCalledTimes(1);
+
+    unsubscribe();
+
+    await emitter.first();
+
+    expect(first).toHaveBeenCalledTimes(1);
+  });
+
+  it('Throwing from within a subscription does not interrupt execution.', async () => {
+    const emitter = createEmitter({
+      first: () => {},
+    });
+
+    const first = vi.fn(() => {
+      throw new Error(':(');
+    });
+
+    emitter.subscribe({ first });
+
+    expect(() => emitter.first()).not.toThrow(':(');
+  });
+
+  it('Supports subscribing to initialize()', async () => {
+    const emitter = createEmitter({
+      async initialize() {},
+    });
+
+    const initialize = vi.fn();
+
+    emitter.subscribe({ initialize });
+
+    await emitter.initialize();
+
+    expect(initialize).toHaveBeenCalledTimes(1);
+  });
+
+  it('Supports enabling/disabling for sync subscriptions', async () => {
+    const emitter = createEmitter({
+      first() {},
+    });
+
+    emitter.disable();
+
+    const first = vi.fn();
+
+    emitter.subscribe({ first });
+
+    await emitter.first();
+
+    expect(first).toHaveBeenCalledTimes(0);
+
+    emitter.enable();
+
+    await emitter.first();
+
+    expect(first).toHaveBeenCalledTimes(1);
+  });
+
+  it('Supports enabling/disabling for async subscriptions', async () => {
+    const emitter = createEmitter({
+      async first() {},
+    });
+
+    emitter.disable();
+
+    const first = vi.fn();
+
+    emitter.subscribe({ first });
+
+    await emitter.first();
+
+    expect(first).toHaveBeenCalledTimes(0);
+
+    emitter.enable();
+
+    await emitter.first();
+
+    expect(first).toHaveBeenCalledTimes(1);
+  });
+});
--- src/create-emitter.ts
+++ src/create-emitter.ts
@@ -29,6 +29,38 @@ import type { Config, Emitter, Subscription, Subscriptions } from './types';
  * logger.error(':(');
  */
 
+/**
+ * @remarks {@link typeOf typeOf()} returns the type of the given value.
+ *
+ * @param {unknown} value The value to check.
+ * @returns array | function | null | number | object | promise | regexp | string | symbol | undefined
+ *
+ * @example
+ *
+ * ```ts
+ * typeOf(''); // string
+ * ```
+ */
+
+export enum Type {
+  Array = 'array',
+  AsyncFunction = 'asyncfunction',
+  Error = 'error',
+  Function = 'function',
+  Null = 'null',
+  Number = 'number',
+  Object = 'object',
+  Promise = 'promise',
+  Regexp = 'regexp',
+  String = 'string',
+  Symbol = 'symbol',
+  Undefined = 'undefined',
+}
+
+export function typeOf(value: unknown): Type {
+  return Object.prototype.toString.call(value).slice(8, -1).toLowerCase() as unknown as Type;
+}
+
 export function createEmitter<T extends Config>(config: T): Emitter<T> {
   const queue: Array<() => Promise<void>> = [];
 
@@ -50,84 +82,124 @@ export function createEmitter<T extends Config>(config: T): Emitter<T> {
   }
 
   function createMethod(key: keyof T & string) {
-    return async function enqueueMethod(...args: Parameters<T[keyof T]>) {
-      return new Promise((resolve, reject) => {
-        async function settle() {
-          try {
-            const fn = config[key];
+    const fn = config[key];
 
-            const result = await fn(...args);
+    switch (typeOf(fn)) {
+      case Type.AsyncFunction:
+        return async function enqueueAsynchronousMethod(...args: Parameters<T[keyof T]>) {
+          return new Promise((resolve, reject) => {
+            async function settle() {
+              try {
+                const result = await fn(...args);
+
+                if (enabled) {
+                  for (const symbol of Object.getOwnPropertySymbols(subscriptions)) {
+                    const subscription = subscriptions[symbol];
+
+                    try {
+                      await Promise.allSettled([
+                        subscription[key]?.(result, ...args),
+                        subscription.all?.<keyof T>(key, result, ...args),
+                      ]);
+                    } catch (error) {
+                      console.error(error);
+                    }
+                  }
+                }
+
+                resolve(result);
+              } catch (error) {
+                for (const symbol of Object.getOwnPropertySymbols(subscriptions)) {
+                  try {
+                    const subscription = subscriptions[symbol];
+                    await subscription?.catch?.<keyof T>(key, error as Error, ...args);
+                  } catch (error) {
+                    console.error(error);
+                  }
+                }
+
+                reject(error);
+              }
+            }
+
+            if (key === 'initialize') {
+              if (initialized) {
+                reject(new Error(`initialize() can only be called once.`));
+                return;
+              } else {
+                initialized = true;
+                queue.unshift(settle);
+              }
+            } else {
+              if (typeOf(config.initialize) === Type.Undefined) {
+                initialized = true;
+              }
+
+              queue.push(settle);
+            }
+
+            if (!initialized || flushing) {
+              return;
+            }
+
+            flushing = true;
+
+            dequeue();
+          });
+        };
+      case Type.Function:
+        return function executeSynchronousMethod(...args: Parameters<T[keyof T]>) {
+          try {
+            if (key === 'initialize') {
+              if (initialized) {
+                throw new Error(`initialize() can only be called once.`);
+              } else {
+                initialized = true;
+              }
+            } else {
+              if (typeOf(config.initialize) === Type.Undefined) {
+                initialized = true;
+              }
+            }
+
+            const result = fn(...args);
 
             if (enabled) {
-              for (const symbol of Object.getOwnPropertySymbols(
-                subscriptions,
-              )) {
-                const subscription = subscriptions[symbol];
-
+              for (const symbol of Object.getOwnPropertySymbols(subscriptions)) {
                 try {
-                  await Promise.allSettled([
-                    subscription[key]?.(result, ...args),
-                    subscription.all?.<keyof T>(key, result, ...args),
-                  ]);
+                  const subscription = subscriptions[symbol];
+                  subscription[key]?.(result, ...args);
+                  subscription.all?.<keyof T>(key, result, ...args);
                 } catch (error) {
                   console.error(error);
                 }
               }
             }
 
-            resolve(result);
+            return result;
           } catch (error) {
             for (const symbol of Object.getOwnPropertySymbols(subscriptions)) {
               try {
                 const subscription = subscriptions[symbol];
-
-                await subscription?.catch?.<keyof T>(
-                  key,
-                  error as Error,
-                  ...args,
-                );
+                subscription.catch?.<keyof T>(key, error as Error, ...args);
               } catch (error) {
                 console.error(error);
               }
             }
 
-            reject(error);
+            throw error;
           }
-        }
+        };
 
-        if (key === 'initialize') {
-          if (initialized) {
-            reject(new Error(`initialize() can only be called once.`));
-            return;
-          } else {
-            initialized = true;
-            queue.unshift(settle);
-          }
-        } else {
-          if (typeof config.initialize === 'undefined') {
-            initialized = true;
-          }
-
-          queue.push(settle);
-        }
-
-        if (!initialized || flushing) {
-          return;
-        }
-
-        flushing = true;
-
-        dequeue();
-      });
-    };
+      default:
+        throw new Error(':(');
+    }
   }
 
   const properties = Object.keys(config).reduce(
     (accumulator, key) => ({
       ...accumulator,
-      [key]: (typeof config[key]).includes('function')
-        ? createMethod(key)
-        : config[key],
+      [key]: typeOf(config[key]).includes(Type.Function) ? createMethod(key) : config[key],
     }),
     {},
   );
